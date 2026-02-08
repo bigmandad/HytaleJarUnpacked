baa2eb80846c8328bc8960acd40720fd66df7876
bigmandad <majied.lafleur@gmail.com>
2026-02-08 15:00:39 -0600

    Add files via upload

---
+++ com/hypixel/hytale/codec/codecs/map/EnumMapCodec$1.java
@@ -0,0 +1,10 @@
+/*     */ package com.hypixel.hytale.codec.codecs.map;
+/*     */ 
+/*     */ import com.hypixel.hytale.codec.codecs.EnumCodec;
+/*     */ 
+
+
+/* Location:              C:\Users\ranor\AppData\Roaming\Hytale\install\release\package\game\latest\Server\HytaleServer.jar!\com\hypixel\hytale\codec\codecs\map\EnumMapCodec$1.class
+ * Java compiler version: 21 (65.0)
+ * JD-Core Version:       1.1.3
+ */
---
+++ com/hypixel/hytale/codec/schema/metadata/Metadata.java
@@ -0,0 +1,13 @@
+package com.hypixel.hytale.codec.schema.metadata;
+
+import com.hypixel.hytale.codec.schema.config.Schema;
+
+public interface Metadata {
+  void modify(Schema paramSchema);
+}
+
+
+/* Location:              C:\Users\ranor\AppData\Roaming\Hytale\install\release\package\game\latest\Server\HytaleServer.jar!\com\hypixel\hytale\codec\schema\metadata\Metadata.class
+ * Java compiler version: 21 (65.0)
+ * JD-Core Version:       1.1.3
+ */
