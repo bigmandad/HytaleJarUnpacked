2905b85c0d9106694e567befb870a426d83096fb
bigmandad <majied.lafleur@gmail.com>
2026-02-09 10:51:10 -0600

    Add files via upload

---
+++ com/hypixel/hytale/server/core/command/system/arguments/system/AbstractOptionalArg$DefaultValueArgument.java
@@ -0,0 +1,11 @@
+package com.hypixel.hytale.server.core.command.system.arguments.system;
+
+public interface DefaultValueArgument<DataType> {
+  DataType getDefaultValue();
+}
+
+
+/* Location:              C:\Users\ranor\AppData\Roaming\Hytale\install\release\package\game\latest\Server\HytaleServer.jar!\com\hypixel\hytale\server\core\command\system\arguments\system\AbstractOptionalArg$DefaultValueArgument.class
+ * Java compiler version: 21 (65.0)
+ * JD-Core Version:       1.1.3
+ */
