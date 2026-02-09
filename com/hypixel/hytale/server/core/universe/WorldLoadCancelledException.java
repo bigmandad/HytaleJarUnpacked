828c8de8b729a5af8866104d67a4814e2e2ffc43
bigmandad <majied.lafleur@gmail.com>
2026-02-09 03:42:00 -0600

    Add files via upload

---
+++ com/hypixel/hytale/server/core/universe/WorldLoadCancelledException.java
@@ -0,0 +1,9 @@
+package com.hypixel.hytale.server.core.universe;
+
+public class WorldLoadCancelledException extends RuntimeException {}
+
+
+/* Location:              C:\Users\ranor\AppData\Roaming\Hytale\install\release\package\game\latest\Server\HytaleServer.jar!\com\hypixel\hytale\server\cor\\universe\WorldLoadCancelledException.class
+ * Java compiler version: 21 (65.0)
+ * JD-Core Version:       1.1.3
+ */
