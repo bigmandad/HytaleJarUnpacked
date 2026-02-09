0c3568c5f191a37670247041449975559665013a
bigmandad <majied.lafleur@gmail.com>
2026-02-09 10:49:34 -0600

    Add files via upload

---
+++ com/hypixel/hytale/server/core/command/system/CommandOwner.java
@@ -0,0 +1,11 @@
+package com.hypixel.hytale.server.core.command.system;
+
+public interface CommandOwner {
+  String getName();
+}
+
+
+/* Location:              C:\Users\ranor\AppData\Roaming\Hytale\install\release\package\game\latest\Server\HytaleServer.jar!\com\hypixel\hytale\server\core\command\system\CommandOwner.class
+ * Java compiler version: 21 (65.0)
+ * JD-Core Version:       1.1.3
+ */
