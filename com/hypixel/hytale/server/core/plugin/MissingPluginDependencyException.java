4753ef6e430b7eae6b6784df874e7a1d08507a92
bigmandad <majied.lafleur@gmail.com>
2026-02-09 03:37:02 -0600

    Add files via upload

---
+++ com/hypixel/hytale/server/core/plugin/MissingPluginDependencyException.java
@@ -0,0 +1,24 @@
+/*    */ package com.hypixel.hytale.server.core.plugin;
+/*    */ 
+/*    */ import javax.annotation.Nonnull;
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ public class MissingPluginDependencyException
+/*    */   extends RuntimeException
+/*    */ {
+/*    */   public MissingPluginDependencyException(@Nonnull String message) {
+/* 16 */     super(message);
+/*    */   }
+/*    */ }
+
+
+/* Location:              C:\Users\ranor\AppData\Roaming\Hytale\install\release\package\game\latest\Server\HytaleServer.jar!\com\hypixel\hytale\server\core\plugin\MissingPluginDependencyException.class
+ * Java compiler version: 21 (65.0)
+ * JD-Core Version:       1.1.3
+ */
---
+++ com/hypixel/hytale/server/core/plugin/commands/PluginCommand$2.java
@@ -0,0 +1,10 @@
+/*     */ package com.hypixel.hytale.server.core.plugin.commands;
+/*     */ 
+/*     */ import com.hypixel.hytale.server.core.plugin.PluginState;
+/*     */ 
+
+
+/* Location:              C:\Users\ranor\AppData\Roaming\Hytale\install\release\package\game\latest\Server\HytaleServer.jar!\com\hypixel\hytale\server\core\plugin\commands\PluginCommand$2.class
+ * Java compiler version: 21 (65.0)
+ * JD-Core Version:       1.1.3
+ */
